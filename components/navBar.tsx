e9f6e7c1e49608bfb83f41cab8fa3adb6e774dfe
Aissam Barchil <abarchil@e1r11p2.1337.ma>
2022-11-23 23:49:26 +0100

    NavBr
    fix mobile-version issue

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -1,170 +1,95 @@
 import Logo from '../assets/Logo.jpg'
 import Image from 'next/image'
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 export default function NavBar() {
+	const navigation = [
+		{ name: 'Home', href: '#', current: true },
+		{ name: 'About', href: '#', current: false },
+		{ name: 'Resume', href: '#', current: false },
+		{ name: 'Services', href: '#', current: false },
+		{ name: 'Skills', href: '#', current: false },
+		{ name: 'Projects', href: '#', current: false },
+		{ name: 'Contact', href: '#', current: false },
+	  ]
+	  function classNames(...classes) {
+		return classes.filter(Boolean).join(' ')
+	  }
   return (
-    <nav className="bg-transparent">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="relative flex h-16 items-center justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            <button
-              type="button"
-              className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-              aria-controls="mobile-menu"
-              aria-expanded="false"
-            >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="block h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                />
-              </svg>
-              <svg
-                className="hidden h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
-          </div>
-          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex flex-shrink-0 items-center">
-			<Image
-			   	src={Logo}
-			    alt="Company logo"
-                className="block h-50 w-20 lg:hidden"
-				height={0}
-				width={0}
+	<Disclosure as="nav" className="bg-transparent-800">
+	{({ open }) => (
+	  <>
+		<div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+		  <div className="relative flex h-16 items-center justify-between">
+			<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+			  <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+				<span className="sr-only">Open main menu</span>
+				{open ? (
+				  <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+				) : (
+				  <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+				)}
+			  </Disclosure.Button>
+			</div>
+			<div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+			  <div className="flex flex-shrink-0 items-center">
+				<Image
+					src={Logo}
+					alt="Company logo"
+					className="block h-50 w-20 lg:hidden"
+					height={0}
+					width={0}
 				/>
-			  <Image
-			   	src={Logo}
-			    alt="Company logo"
-                className="hidden h-50 w-20 lg:block"
-				height={0}
-				width={0}
+				<Image
+					src={Logo}
+					alt="Company logo"
+					className="hidden h-50 w-20 lg:block"
+					height={0}
+					width={0}
 				/>
-            </div>
-            <div className="hidden sm:ml-6 sm:block">
-              <div className="flex space-x-4">
-                <a
-                  href="#"
-                  className="text-white px-3 py-2 rounded-md text-sm font-medium"
-                  aria-current="page"
-                >
-                  Home
-                </a>
-
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  About
-                </a>
-
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Resume
-                </a>
-
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Services
-                </a>
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Skills
-                </a>
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Projects
-                </a>
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Contact
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="sm:hidden" id="mobile-menu">
-        <div className="space-y-1 px-2 pt-2 pb-3">
-          <a
-            href="#"
-            className=" text-white block px-3 py-2 rounded-md text-base font-medium"
-            aria-current="page"
-          >
-            Home
-          </a>
-
-          <a
-            href="#"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-          >
-            About
-          </a>
-
-          <a
-            href="#"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-          >
-            Resume
-          </a>
-
-          <a
-            href="#"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-          >
-            Services
-          </a>
-          <a
-            href="#"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-          >
-            Skills
-          </a>
-          <a
-            href="#"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-          >
-            Projects
-          </a>
-          <a
-            href="#"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-          >
-            Contact
-          </a>
-        </div>
-      </div>
-    </nav>
+			  </div>
+			  <div className="hidden sm:ml-6 sm:block">
+				<div className="flex space-x-4">
+				  {navigation.map((item) => (
+					<a
+					  key={item.name}
+					  href={item.href}
+					  className={classNames(
+						'text-gray-300 hover:bg-fav-color hover:text-white',
+						'px-3 py-2 rounded-md text-sm font-medium'
+					  )}
+					  aria-current={item.current ? 'page' : undefined}
+					>
+					  {item.name}
+					</a>
+				  ))}
+				</div>
+			  </div>
+			</div>
+		
+		  </div>
+		</div>
+		<Disclosure.Panel className="sm:hidden">
+		  <div className="space-y-1 px-2 pt-2 pb-3">
+			{navigation.map((item) => (
+			  <Disclosure.Button
+				key={item.name}
+				as="a"
+				href={item.href}
+				className={classNames(
+				  'text-gray-300 hover:bg-fav-color hover:text-white',
+				  'block px-3 py-2 rounded-md text-base font-medium'
+				)}
+				aria-current={item.current ? 'page' : undefined}
+			  >
+				{item.name}
+			  </Disclosure.Button>
+			))}
+		  </div>
+		</Disclosure.Panel>
+	  </>
+	)}
+  </Disclosure>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
 		"./components/**/*.{js,ts,jsx,tsx}",
 	],
   	theme: {
-    extend: {},
+    extend: {
+		colors: {
+			'fav-color': '#00FFF9',
+		  },
+	},
 	},
 	plugins: [],
 }
