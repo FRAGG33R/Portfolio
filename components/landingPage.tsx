6967117880fe9a64866a0b28b9a9e5ce153e8f72
Aissam Barchil <abarchil@e1r11p4.1337.ma>
2022-11-24 22:43:38 +0100

    Landing page
    add animation to the text (Hi....)
    
    switch cursor color

--- components/landingPage.tsx
+++ components/landingPage.tsx
@@ -10,21 +10,28 @@ export default function LandingPage()
 				<div className="flex self-start justify-center">
 					<Image className={styles.Img} src={Me} width={480} height={480}/>
 				</div>
-				<div className="flex justify-center items-center h-4/6 flex-wrap font-mono subpixel-antialiased text-4xl font-semibold tracking-wide text-left">
-					<p className="text-white">Hi, I'm <span className="text-fav-color">Aissam</span></p>
-					{/* <Typewriter
-						options={{
-							autoStart: true,
-							loop: true,
-						}}
-						onInit={(typewriter) => {
-						typewriter.typeString('a web3 developer')
-						.pauseFor(2500)
-						.deleteAll()
-						typewriter.typeString('a blockchain enthusiast')
-						.start();
-						}}
-					/> */}
+				<div className="flex md:justify-center items-center flex-col h-4/6 flex-wrap font-mono text-white sm:text-3xl subpixel-antialiased font-semibold tracking-wide text-left">
+					<p>Hi, I'm <span className="text-fav-color">Aissam</span></p>
+					<div>
+						<Typewriter
+							options={{
+								autoStart: true,
+								loop: true,
+								cursorClassName: "text-fav-color",
+							}}
+							onInit={(typewriter) => {
+								typewriter.typeString('web developer.')
+								.pauseFor(2200)
+								.deleteAll()
+								typewriter.typeString('web3 researcher.')
+								.pauseFor(2200)
+								.deleteAll()
+								typewriter.typeString('blockchain enthusiast.')
+								.start();
+							}}
+						/>
+					</div>
+					{/* <p>blockchain enthusiast</p> */}
 				</div>
 		</div>
 	);
