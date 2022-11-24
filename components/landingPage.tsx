67bc06d5ca0c5446728a30fab246024db00041a8
Aissam Barchil <abarchil@e1r11p4.1337.ma>
2022-11-24 23:46:07 +0100

    landing page
    trying to solve responsive problem with type effect

--- components/landingPage.tsx
+++ components/landingPage.tsx
@@ -10,7 +10,7 @@ export default function LandingPage()
 				<div className="flex self-start justify-center">
 					<Image className={styles.Img} src={Me} width={480} height={480}/>
 				</div>
-				<div className="flex md:justify-center items-center flex-col h-4/6 flex-wrap font-mono text-white sm:text-3xl subpixel-antialiased font-semibold tracking-wide text-left">
+				<div className="flex justify-center items-center flex-col h-4/6 md:w-2/4 xl:w-3/12 flex-wrap font-mono text-white lg:text-3xl sm:text-xl ubpixel-antialiased font-semibold tracking-wide text-left">
 					<p>Hi, I'm <span className="text-fav-color">Aissam</span></p>
 					<div>
 						<Typewriter
@@ -18,15 +18,19 @@ export default function LandingPage()
 								autoStart: true,
 								loop: true,
 								cursorClassName: "text-fav-color",
+								delay: 65,
 							}}
 							onInit={(typewriter) => {
 								typewriter.typeString('web developer.')
-								.pauseFor(2200)
+								.pauseFor(2000)
+								.changeDeleteSpeed(65)
 								.deleteAll()
 								typewriter.typeString('web3 researcher.')
-								.pauseFor(2200)
+								.pauseFor(2000)
+								.changeDeleteSpeed(65)
 								.deleteAll()
 								typewriter.typeString('blockchain enthusiast.')
+								.pauseFor(2000)
 								.start();
 							}}
 						/>
