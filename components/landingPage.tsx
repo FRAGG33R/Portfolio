109640baba44655f46b83092ea066c58edcecbdd
Aissam Barchil <abarchil@e1r13p5.1337.ma>
2022-11-26 18:32:04 +0100

    landing page
    fix all landing page responsivity problems
    change font family

--- components/landingPage.tsx
+++ components/landingPage.tsx
@@ -9,8 +9,8 @@ import Linkedin from '../assets/linkedin.gif'
 import Twitter from '../assets/twitter.gif'
 export default function LandingPage() {
 	return (
-		<div className="flex lg:flex-row xs:flex-col-reverse lg:justify-center items-center h-4/5 w-screen">
-				<div className="xs:w-full lg:w-1/2 h-full lg:flex lg:items-center lg:justify-center">
+		<div className="flex lg:flex-row xs:flex-col-reverse lg:justify-center items-center h-4/5 w-screen 2xl:bg-red-500">
+				<div className="xs:w-full lg:w-1/2 xl:w-2/6 h-full flex flex-col items-center justify-center lg:text-5xl xs:text-3xl text-white font-Bai">
 					<p>Hi, I'm <span className="text-fav-color">Aissam</span></p>
 							<div>
 								<Typewriter
@@ -18,7 +18,7 @@ export default function LandingPage() {
 										autoStart: true,
 										loop: true,
 										cursorClassName: "text-fav-color",
-										delay: 65,
+										delay: 110,
 									}}
 									onInit={(typewriter) => {
 										typewriter.typeString('web developer.')
@@ -37,20 +37,20 @@ export default function LandingPage() {
 							</div>
 							<div className="flex flex-row flex-wrap pt-32">
 								<div>
-									<Image src={Github} width={100} height={100}/>
+									<Image alt="Github icon" src={Github} width={100} height={100}/>
 								</div>
 								<div>
-									<Image src={Linkedin} width={100} height={100}/>
+									<Image alt="Linkedin icon" src={Linkedin} width={100} height={100}/>
 								</div>
 								<div>
-									<Image src={Twitter} width={100} height={100}/>
+									<Image alt="Twitter icon" src={Twitter} width={100} height={100}/>
 								</div>
 							</div>
 						<div>
 					</div>
 				</div>
-				<div className="xs:w-full lg:w-1/2 h-full flex items-center justify-center">
-					<Image className={styles.Img} src={Me} width={450} height={450}/>
+				<div className="xs:w-full lg:w-1/2 xl:w-2/6 h-full flex items-center justify-center">
+					<Image alt="image" className={styles.Img} src={Me} width={550} height={550}/>
 				</div>
 		</div>
 	);
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,16 @@ module.exports = {
 		colors: {
 			'fav-color': '#00ccc2',
 		  },
+		  fontFamily: {
+			'Bai': ['Bai Jamjuree', 'sans-serif']
+		  },
 	},
 	screens: {
 		'xs': '100px',
 		'lg': '1024px',
 		'sm': '640px',
     	'md': '768px',
+		'xl': '2000px',
 	  },
 	},
 	plugins: [],
