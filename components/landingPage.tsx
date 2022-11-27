5128f212571268f93291c1fff35bdc78c675c7c0
Aissam Barchil <abarchil@e1r13p4.1337.ma>
2022-11-27 18:48:48 +0100

    landign page
    add animation to landing page using framer motion library

--- components/landingPage.tsx
+++ components/landingPage.tsx
@@ -2,42 +2,56 @@ import Image from "next/image"
 import Me from '../assets/Me_2.jpg'
 import styles from '../styles/landingPage.module.css'
 // import Spline from '@splinetool/react-spline';
+import { motion } from "framer-motion";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { AnimatePresence } from "framer-motion";
 import { useInView } from "framer-motion";
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { useRef } from "react"
 import Typewriter from 'typewriter-effect';
+import Link from "next/link"
 export default function LandingPage() {
-	function Section({ children }) {
-		const ref = useRef(null);
-		const isInView = useInView(ref, { once: true });
-	  
-		return (
-		  <section ref={ref}>
-			<span
-			  style={{
-				transform: isInView ? "none" : "translateX(-200px)",
-				opacity: isInView ? 1 : 0,
-				transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
-			  }}
-			>
-			  {children}
-			</span>
-		  </section>
-		);
-	  }
+	const container = {
+		hidden: { opacity: 1, scale: 0 },
+			visible: {
+			opacity: 1,
+			scale: 1,
+			transition: {
+				delayChildren: 0.1,
+				staggerChildren: 0.22
+			}
+		}
+	};
+	const item = {
+		hidden: { x : -150, opacity: 0 },
+		visible: {
+			x: 0,
+			opacity: 1
+		}
+	};
+	const __img = {
+		hidden: { x : 250, opacity: 0 },
+		visible: {
+			x: 0,
+			opacity: 1
+		}
+	};
 	return (
-		<div className="flex lg:flex-row xs:flex-col-reverse lg:justify-center items-center h-4/5 w-screen 2xl:bg-red-500">
-			<div className="xs:w-full lg:w-1/2 xl:w-2/6 h-full flex flex-col items-center justify-center lg:text-5xl xs:text-3xl text-white font-Bai">
-				<div className="text-white md:text-lg xs:text-xs font-extralight md:pb-5">
+		<motion.div
+			variants={container}
+			initial="hidden"
+			animate="visible"
+			className="flex lg:flex-row xs:flex-col-reverse lg:justify-center items-center h-4/5 w-screen 2xl:bg-red-500">
+			<motion.div	className="xs:w-full lg:w-1/2 xl:w-2/6 h-full flex flex-col items-center justify-center lg:text-5xl xs:text-3xl text-white font-Bai">
+				<motion.div variants={item} className="text-white md:text-lg xs:text-xs font-extralight md:pb-5">
 					<p>WELCOME TO MY WORLD</p>
-				</div>
-				<div className="text-white text-left">
+				</motion.div>
+				<motion.div variants={item} className="text-white text-left">
 					<p>Hi, I'm <span className="text-fav-color">Aissam</span></p>
-				</div>
-				<div className="md:pb-5">
+				</motion.div>
+				<motion.div variants={item} className="md:pb-5">
 					<Typewriter
 						options={{
 							autoStart: true,
@@ -59,27 +73,33 @@ export default function LandingPage() {
 							.start();
 						}}
 					/>
-				</div>
-				<div className="text-white xs:text-xs md:text-lg lg:text-xl xl:w-[700px] lg:w-[500px] md:w-[500px] xs:w-[300px] font-light  xl:text-center xs:text-left md:pb-5 tracking-widest pt-6">
+				</motion.div>
+				<motion.div variants={item} className="text-white xs:text-xs md:text-lg lg:text-xl xl:w-[700px] lg:w-[500px] md:w-[500px] xs:w-[300px] font-light  xl:text-center xs:text-left md:pb-5 tracking-widest pt-6">
 					<p>
 						Im a Web developer specialized in front-end. I'm also passionate about the newest technologies such as blockchain, web3, smart contracts, decentralized applications.
 					</p>
-				</div>
-				<div className="flex flex-row  justify-center items-center pt-6">
+				</motion.div>
+				<motion.div variants={item} className="flex flex-row  justify-center items-center pt-6">
 					<div className="h-20 w-20">
-						<FontAwesomeIcon className="hover:text-fav-color w-9 h-9" icon={faGithub} />
+						<Link href="https://github.com/FRAGG33R" target="_blank">
+							<FontAwesomeIcon className="hover:text-fav-color w-9 h-9" icon={faGithub} />
+						</Link>
 					</div>
 					<div className="h-20 w-20">
-						<FontAwesomeIcon className="hover:text-fav-color w-9 h-9" icon={faLinkedin} />
+						<Link href="https://www.linkedin.com/in/aissam-barchil-846408211/" target="_blank">
+							<FontAwesomeIcon className="hover:text-fav-color w-9 h-9" icon={faLinkedin} />
+						</Link>
 					</div>
 					<div className="h-20 w-20">
-						<FontAwesomeIcon className="hover:text-fav-color w-9 h-9" icon={faTwitter} />
+						<Link href="https://twitter.com/FR4_GG3R" target="_blank">
+							<FontAwesomeIcon className="hover:text-fav-color w-9 h-9" icon={faTwitter} />
+						</Link>
 					</div>
-				</div> 
-			</div>
-				<div className="xs:w-full lg:w-1/2 xl:w-2/6 h-full flex items-center justify-center pb-6">
-					<Image alt="image" className={styles.Img} src={Me} width={550} height={550}/>
-				</div>
-		</div>
+				</motion.div> 
+			</motion.div>
+			<motion.div variants={__img} className="xs:w-full lg:w-1/2 xl:w-2/6 h-full flex items-center justify-center pb-6">
+				<Image alt="image" className={styles.Img} src={Me} width={550} height={550}/>
+			</motion.div>
+		</motion.div>
 	);
 }
