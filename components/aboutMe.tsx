55965517b3da6d57dc0e3c526c1f7299ed1eac87
Aissam Barchil <abarchil@e1r13p5.1337.ma>
2022-11-29 11:47:37 +0100

    About me component
    add about me structure
    brfore styling...

--- components/aboutMe.tsx
+++ components/aboutMe.tsx
@@ -6,8 +6,13 @@ export default function AboutMe(){
 			<div className="xs:w-full lg:w-1/2 xl:w-2/6 h-full flex flex-col items-center justify-center lg:text-5xl xs:text-3xl text-white font-Bai ">
 				<Spline scene="https://prod.spline.design/vG42Fqg6duViAFup/scene.splinecode" />
 			</div>
-			<div className="xs:w-full lg:w-1/2 xl:w-2/6 h-full flex items-center justify-center pb-6">
-
+			<div className="xs:w-full lg:w-1/2 xl:w-2/6 h-full flex items-center justify-center space-y-14 text-left flex-col pb-6 text-white">
+				<div className='text-4xl'>About Me</div>
+				<div>small desciption Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate odit iusto rem nam cum quam facere dignissimos corporis, adsfsdf</div>
+				<div><span>Name: </span> Aissam barchil</div>
+				<div><span>Date of birth: </span> September 17, 2002</div>
+				<div><span>Email: </span>aissambarchil70@gmail.com</div>
+				<div><span>Phone: </span>+212625386430</div>
 			</div>
 		</div>
 	)
